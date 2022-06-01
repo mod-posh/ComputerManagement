aeb26eae156851923be845fdf47988408ac8e880
Jeffrey Patton <jeffrey@patton-tech.com>
2022-05-31 22:42:36 -0500

    issu-68: Moved Get-WinEventTail

--- ComputerManagement/ComputerManagement.psm1
+++ ComputerManagement/ComputerManagement.psm1
@@ -572,33 +572,3 @@ Function Get-NetShare {
   End {
   }
 }
-Function Get-WinEventTail {
-  [CmdletBinding(HelpURI = 'https://github.com/mod-posh/ComputerManagement/blob/master/docs/Get-WinEventTail.md#get-wineventtail')]
-  Param
-  (
-    [string]$LogName = 'System',
-    [int]$ShowExisting = 10
-  )
-  Begin {
-    if ($ShowExisting -gt 0) {
-      $Data = Get-WinEvent -LogName $LogName -MaxEvents $ShowExisting
-      $Data | Sort-Object -Property RecordId
-      $Index1 = $Data[0].RecordId
-    }
-    else {
-      $Index1 = (Get-WinEvent -LogName $LogName -MaxEvents 1).RecordId
-    }
-  }
-  Process {
-    while ($true) {
-      Start-Sleep -Seconds 1
-      $Index2 = (Get-WinEvent -LogName $LogName -MaxEvents 1).RecordId
-      if ($Index2 -gt $Index1) {
-        Get-WinEvent -LogName $LogName -MaxEvents ($Index2 - $Index1) | Sort-Object -Property RecordId
-      }
-      $Index1 = $Index2
-    }
-  }
-  End {
-  }
-}
