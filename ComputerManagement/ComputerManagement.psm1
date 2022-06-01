4cf9f9a66bbd01a29934be52ae1248283bd7df8f
Jeffrey Patton <jeffrey@patton-tech.com>
2022-06-01 10:29:25 -0500

    issue-68: Moved Connect-Rdp

--- ComputerManagement/ComputerManagement.psm1
+++ ComputerManagement/ComputerManagement.psm1
@@ -506,33 +506,3 @@ Function New-Password {
     Return $Passwords
   }
 }
-function Connect-Rdp {
-  [CmdletBinding(HelpURI = 'https://github.com/mod-posh/ComputerManagement/blob/master/docs/Connect-Rdp.md#connect-rdp')]
-  param
-  (
-    [Parameter(Mandatory = $true, ValueFromPipeline = $True)]
-    $ComputerName,
-    [pscredential]$Credential
-  )
-  Process {
-    # take each computername and process it individually
-    Foreach ($Computer in $ComputerName) {
-      # if the user has submitted a credential, store it
-      # safely using cmdkey.exe for the given connection
-      if ($PSBoundParameters.ContainsKey('Credential')) {
-        # extract username and password from credential
-        $User = $Credential.UserName
-        $Password = $Credential.GetNetworkCredential().Password
-
-        # save information using cmdkey.exe
-        cmdkey.exe /generic:$Computer /user:$User /pass:$Password
-      }
-      # initiate the RDP connection
-      # connection will automatically use cached credentials
-      # if there are no cached credentials, you will have to log on
-      # manually, so on first use, make sure you use -Credential to submit
-      # logon credential
-      mstsc.exe /v $Computer /f
-    }
-  }
-}
