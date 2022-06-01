6814e7d4c06efdd35902594cf780fac52ba77ab7
Jeffrey Patton <jeffrey@patton-tech.com>
2022-06-01 10:48:36 -0500

    issue-68: Moved Get-NonStandardServiceAccount

---
+++ ComputerManagement/public/Get-NonStandardServiceAccount.ps1
@@ -0,0 +1,33 @@
+Function Get-NonStandardServiceAccount {
+ [CmdletBinding(HelpURI = 'https://github.com/mod-posh/ComputerManagement/blob/master/docs/Get-NonStandardServiceAccount.md#get-nonstandardserviceaccount')]
+ Param
+ (
+  [string]$Computer = (& hostname),
+  [pscredential]$Credentials,
+  [string]$Filter = "localsystem|NT Authority\LocalService|NT Authority\NetworkService"
+ )
+ Begin {
+  $Filter = $Filter.Replace("\", "\\")
+ }
+ Process {
+  If ($Computer -eq (& hostname)) {
+   $Services = Get-CimInstance -ClassName Win32_Service | Select-Object __Server, StartName, Name, DisplayName
+  }
+  Else {
+   $Result = Test-Connection -Count 1 -Computer $Computer -ErrorAction SilentlyContinue
+
+   If ($null -ne $result) {
+    $Services = Get-CimInstance -ClassName Win32_Service -ComputerName $Computer -Credential $Credentials `
+    | Select-Object __Server, StartName, Name, DisplayName
+   }
+   Else {
+    #	Should do something with unreachable computers here.
+   }
+  }
+
+  $Suspect = $Services | Where-Object { $_.StartName -notmatch $Filter }
+ }
+ End {
+  Return $Suspect
+ }
+}
